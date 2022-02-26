b537dd9232501d319bb8baf454fa77554e1dfab3
jack ponder <jack@ponder-it.com>
2022-02-26 14:38:47 -0500

    main project  push

---
+++ matchthediffVR/Source/matchthediffVR/matchthediffVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "matchthediffVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATCHTHEDIFFVR_API AmatchthediffVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

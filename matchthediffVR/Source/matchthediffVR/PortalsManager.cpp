ab8545817063e5d934ab2163be2267c6b800602d
Jacob Ponder <jack@ponder-it.com>
2022-03-13 04:08:40 -0400

    small pushes

---
+++ matchthediffVR/Source/matchthediffVR/PortalsManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PortalsManager.h"
+
+// Sets default values
+APortalsManager::APortalsManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APortalsManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APortalsManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+

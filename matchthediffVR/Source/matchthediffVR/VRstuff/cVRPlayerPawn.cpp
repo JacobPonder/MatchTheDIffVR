127994012d2e15e1ff7197ef21b85cf3e8b1c5fc
Jacob Ponder <jack@ponder-it.com>
2022-05-12 15:54:46 -0400

    accidental uncomment

--- matchthediffVR/Source/matchthediffVR/VRstuff/cVRPlayerPawn.cpp
+++ matchthediffVR/Source/matchthediffVR/VRstuff/cVRPlayerPawn.cpp
@@ -105,7 +105,7 @@ void AcVRPlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 	InputComponent->BindAction("GripLeft", IE_Released, this, &AcVRPlayerPawn::GripLeftHand_Released);
 	InputComponent->BindAction("GripRight", IE_Released, this, &AcVRPlayerPawn::GripRightHand_Released);
 	//InputComponent->BindAction("unassigned left grip", IE_Pressed, this, );
-	InputComponent->BindAction("TP", IE_Pressed, this, );
+	//InputComponent->BindAction("TP", IE_Pressed, this, );
 	//InputComponent->BindAction("unassigned left grip", IE_Released, this, );
 	InputComponent->BindAction("TP", IE_Released, this, &AcVRPlayerPawn::TP_Player);
 	InputComponent->BindAction("Swap Houses", IE_Released, this, &AcVRPlayerPawn::TP_Houses);
